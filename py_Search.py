a9230c66e1e0e7a4a4f385bef7bf926ccb36677f
smoldash <smoldash@gmail.com>
2015-11-12 22:29:23 +0300

    third commit

---
+++ py_Search.py
@@ -0,0 +1,13 @@
+import pelopia
+from pelopia import Dataset
+from pelopia import GeoJSON
+
+ds = Dataset("NYC.pelopia")
+resp = ds.Search("Mapzen", LatLon(40.7507545 -73.9939152))
+index = input()
+id = resp.get(index)     #if return id, score return will be obj
+Feature place = ds.Place(id)
+print(place.Name, place.Address)
+
+if __name__ == '__main__':
+	main()
